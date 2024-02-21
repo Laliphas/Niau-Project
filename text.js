72be876a4200846780aa8644677c7125c2d9e8bf
Paphaswalan <paphas.teera@gmail.com>
2024-02-21 23:46:49 +0700

    test

---
+++ text.js
@@ -0,0 +1,5 @@
+// TensorFlow.js (Optional):
+// If you choose a TensorFlow-based approach for lipstick overlay or pre-trained lipstick segmentation models, TensorFlow.js is necessary in Node.js.
+// Frontend Interaction:
+// UI framework/library (e.g., React) for creating the user interface.
+// State management (e.g., Redux or Context API) if dealing with complex application state.
