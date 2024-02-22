1efa770d4d383c553e358da6384431795b621811
KanjanapanTakamwan <154393255+KanjanapanTakamwan@users.noreply.github.com>
2024-02-22 22:42:33 +0700

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,5 @@
 import "@/styles/globals.css";
-import { Inter } from 'next/font/google'
-const inter = Inter({ subsets: ['latin'] })
- 
+
 export default function App({ Component, pageProps }) {
-  <main className={inter.className}>
-  <Component {...pageProps} />
-</main>
+  return <Component {...pageProps} />;
 }
