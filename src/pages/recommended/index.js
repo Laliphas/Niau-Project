a41c6c17ca9c45e8e81884c18d283021daa520e1
MildPattranis <154392957+MildPattranis@users.noreply.github.com>
2024-02-23 01:15:06 +0700

    Update index.js

--- src/pages/recommended/index.js
+++ src/pages/recommended/index.js
@@ -38,50 +38,53 @@ export default function Warm() {
   }, [tone, productsData]);
 
   return (
-    <div className={styles.body}>
+    <>
       <Navigator />
-      <div className={styles.header}>
-        <h1>Recommended</h1>
-      </div>
-      <div className={styles.head}>
-        <div className={styles.headBox}>
-          for&nbsp;<a className={styles.select} onClick={handleClick}>{tone} tone
-          </a>
-          {
-            dropdown ?
-              <div className={styles.option}>
-                <button onClick={() => handleChange('warm')}>Warm Tone</button>
-                <button onClick={() => handleChange('neutral')}>Natural Tone</button>
-                <button onClick={() => handleChange('cool')}>Cool Tone</button>
-              </div> : null
-          }
+      <div className={styles.body}>
+        <div className={styles.header}>
+          <h1>Recommended</h1>
+        </div>
+        <div className={styles.head}>
+          <div className={styles.headBox}>
+            for&nbsp;<a className={styles.select} onClick={handleClick}>{tone} tone
+            </a>
+            {
+              dropdown ?
+                <div className={styles.option}>
+                  <button onClick={() => handleChange('warm')}>Warm Tone</button>
+                  <button onClick={() => handleChange('neutral')}>Natural Tone</button>
+                  <button onClick={() => handleChange('cool')}>Cool Tone</button>
+                </div> : null
+            }
+          </div>
+
         </div>
 
+
+
+        <div className={styles.products}>
+          {filterData.map((product) => (
+
+            <Link key={product.id} href={`/productDetails/${product?.model}`} className={styles.box}>
+              <div className={styles.boximg}>
+                <img className={styles.img} loading='lazy' src={`${process.env.NEXT_PUBLIC_API_BASE_URL_IMAGE}/${product?.image}.png`} alt="image" />
+              </div>
+              <div className={styles.content}>
+                <div>
+                  {product?.brand}
+                </div>
+                <div>
+                  Model : {product?.model}
+                </div>
+                <div>
+                  $ {product?.price} / pc
+                </div>
+              </div>
+            </Link>
+          ))}
+        </div>
       </div>
+    </>
 
-
-
-      <div className={styles.products}>
-        {filterData.map((product) => (
-
-          <Link key={product.id} href={`/productDetails/${product?.model}`} className={styles.box}>
-            <div className={styles.boximg}>
-              <img className={styles.img} loading='lazy' src={`${process.env.NEXT_PUBLIC_API_BASE_URL_IMAGE}/${product?.image}.png`} alt="image" />
-            </div>
-            <div className={styles.content}>
-              <div>
-                {product?.brand}
-              </div>
-              <div>
-                Model : {product?.model}
-              </div>
-              <div>
-                $ {product?.price} / pc
-              </div>
-            </div>
-          </Link>
-        ))}
-      </div>
-    </div>
   );
 }
